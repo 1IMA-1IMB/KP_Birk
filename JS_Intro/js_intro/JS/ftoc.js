2674231643fdafdd9953f9680f7fa78d76c7127b
BirkLJ <148325854+BirkLJ@users.noreply.github.com>
2024-01-15 09:34:12 +0100

    farheheit to celcius

---
+++ JS_Intro/js_intro/JS/ftoc.js
@@ -0,0 +1,5 @@
+let farhenheit = 45;
+
+let celsius = (farhenheit - 32) / 1.8;
+
+console.log(celsius);
---
+++ JS_Intro/js_intro/JS/index.js
@@ -0,0 +1,5 @@
+//Denne koden er en kommentar i javascript
+console.log('Hello World');
+let navn = "Birk"
+let alder = "16"
+
